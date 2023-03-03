570b2ecbc266417956ca3449c1b0f2b4987c5af5
Sepehr Raisi <sepehr.raisi@gmail.com>
2023-03-03 22:32:56 +0330

    Initial commit

---
+++ core/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from .views import (
+    home_view,
+    about_view,
+    home_ex_view,
+    TeammateRequestFormView,
+    GymRequestFormView,
+    CoachRequestFormView
+)
+
+app_name = 'core'
+urlpatterns = [
+    path('', home_view, name='home'),
+    path('about/', about_view, name='about'),
+    path('ex/', home_ex_view, name='exhome'),
+    path('teammate-request/', TeammateRequestFormView.as_view(), name='teammate-request'),
+    path('gym-request/', GymRequestFormView.as_view(), name='gym-request'),
+    path('coach-request/', CoachRequestFormView.as_view(), name='coach-request'),
+]
